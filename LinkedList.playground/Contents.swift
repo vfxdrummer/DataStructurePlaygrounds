d95b58520ef805852831ee4f0f536d7768d51b42
vfxdrummer <timothy.brandt@gmail.com>
2017-03-08 12:13:11 -0800

    Update

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,5 +1,109 @@
-//: Playground - noun: a place where people can play
+//
+// LinkedList
+//
+//
+//
 
-import UIKit
+public class LinkedList<T : Equatable> {
+  var value:T
+  var next:LinkedList?
+  
+  init(value:T) {
+    self.value = value
+    self.next = nil
+  }
+  
+  public convenience init(array: [T]) {
+    precondition(array.count > 0)
+    self.init(value: array.first!)
+    var node = self
+    for v in array.dropFirst() {
+      let newNode = LinkedList<T>(value: v)
+      node.addNode(node: newNode)
+      node = newNode
+    }
+  }
+  
+  func addNode(node:LinkedList<T>) {
+    next = node
+  }
+  
+  func findPreviousNode(value:T) -> LinkedList? {
+    var node:LinkedList? = self
+    var previous:LinkedList? = nil
+    while (node != nil) {
+      if (node!.value == value) {
+        return previous
+      }
+      previous = node
+      node = node?.next
+    }
+    return nil
+  }
+  
+  func findNode(value:T) -> LinkedList? {
+    var node:LinkedList? = self
+    while (node != nil) {
+      if (node!.value == value) {
+        return node
+      }
+      node = node?.next
+    }
+    return nil
+  }
+  
+  func copyNode(node:LinkedList?) {
+    if (node == nil) { return }
+    self.value = node!.value
+    self.next = node!.next
+  }
+  
+  func deleteNode(value:T) {
+    // assumes that self is root
+    let node = findPreviousNode(value: value)
+    if (node == nil) {
+      if (next != nil) {
+        // copy next node over root
+        self.copyNode(node: next!)
+      }
+      return
+    }
+    // set pointer to jump node to delete
+    // leave deleted node to be cleaned up by ARC
+    node?.next = node?.next?.next
+  }
+  
+  func display() -> String {
+    var node:LinkedList? = self
+    var displayString = ""
+    while (node != nil) {
+      print("")
+      displayString += "\(node!.value) -> "
+      node = node?.next
+    }
+    return displayString
+  }
+}
 
-var str = "Hello, playground"
+
+// create linkedlist nodes
+let n1 = LinkedList<Int>(value: 7)
+print(n1.value)
+// add node
+n1.addNode(node:LinkedList<Int>(value: 9))
+n1.display()
+n1.findNode(value: 9)?.value
+n1.findPreviousNode(value: 9)?.value
+n1.deleteNode(value: 9)
+n1.display()
+let n2 = LinkedList<Int>(array:[1,2,3,4,5,6,7,8])
+n2.display()
+n2.deleteNode(value: 4)
+n2.display()
+n2.deleteNode(value: 1)
+n2.display()
+
+
+
+
+
--- NonAttackingQueens.playground/Contents.swift
+++ NonAttackingQueens.playground/Contents.swift
@@ -1,5 +1,85 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+class ChessBoard {
+  var positions = [(Int, Int)]()
+  
+  init() {
+  }
+  
+  public convenience init(cB: ChessBoard) {
+    self.init()
+    self.positions = cB.positions
+  }
+  
+  func numQueens() -> Int {
+    return positions.count
+  }
+  
+  func checkPosition(r:Int, c:Int) -> Bool {
+    for position in positions {
+      if (position.0 == r && position.1 == c) {
+        return true
+      }
+    }
+    return false
+  }
+  
+  func getAvailableCols() -> [Int] {
+    var cols:[Int] = []
+    for i in 0..<8 {
+      var add = true
+      for position in positions {
+        if position.0 == i {
+          add = false
+        }
+      }
+      if add == true {
+        cols.append(i)
+      }
+    }
+    
+    return cols
+  }
 
-var str = "Hello, playground"
+  func placeQueen(r:Int, c:Int) {
+    let cols = getAvailableCols()
+    if (numQueens() < 8 && cols.contains(c)) {
+      positions.append((c,r))
+    }
+  }
+  
+  func nonAttacking(cB:ChessBoard, row:Int) {
+    if (row >= 8) {
+      cB.description()
+      return
+    }
+    
+    print (cB.getAvailableCols())
+    for col in cB.getAvailableCols() {
+      let cBNew:ChessBoard = ChessBoard(cB: cB)
+      cBNew.placeQueen(r: row, c: col)
+      cBNew.nonAttacking(cB: cBNew, row: row + 1)
+    }
+  }
+  
+  func description() {
+    for j in 0..<8 {
+      var descriptionString = ""
+      for i in 0..<8 {
+        descriptionString += (checkPosition(r: i, c: j)) ? "O " : "X "
+      }
+      print(descriptionString)
+    }
+    print("\n")
+  }
+}
+
+// tests
+let c = ChessBoard()
+c.description()
+c.getAvailableCols()
+//c.placeQueen(r: 0, c: 0)
+c.getAvailableCols()
+// lots of combination & recursion, uncomment @ own peril!!!!
+//c.nonAttacking(cB: c, row: 0)
+
