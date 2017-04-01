4a6930e208ed623f3289356e5faef50ed7981901
vfxdrummer <timothy.brandt@gmail.com>
2017-04-01 16:28:46 -0700

    Add Test 04.01.17

--- LRUCacheSimple.playground/Contents.swift
+++ LRUCacheSimple.playground/Contents.swift
@@ -94,3 +94,104 @@ l.describe()
 l.insert(key: "seven", value: "4", cost: 5.0)
 l.describe()
 
+
+// Test 04.01.17
+// LRUCache
+//
+
+// Api
+//
+// Node - value : cost
+//
+// Dictionary - K : V
+//    total cost
+//    exict when totalcost exceeds maximum
+//
+
+class Node<K,V:Comparable> {
+    var key:K
+    var value:V
+    var cost:Int
+    
+    init(key:K, value:V, cost:Int) {
+        self.key = key
+        self.value = value
+        self.cost = cost
+    }
+    
+    func describe() {
+        print("[key : \(key), value : \(value), cost : \(cost)]")
+    }
+}
+
+class LRUCache<K:Hashable,V:Comparable> {
+    var hash: [K : Node<K,V>]
+    var lastUsed: [Node<K,V>]
+    var maxCost: Int
+    
+    init(maxCost: Int) {
+        self.hash = [:]
+        self.lastUsed = []
+        self.maxCost = maxCost
+    }
+    
+    public func insert(key:K, value:V, cost:Int) {
+        let node = Node<K,V>(key:key, value:value, cost:cost)
+        lastUsed.append(node)
+        hash[key] = node
+        checkCost()
+    }
+    
+    
+    private func totalCost() -> Int {
+        var totalCost:Int = 0
+        for node in lastUsed {
+            totalCost += node.cost
+        }
+        return totalCost
+    }
+    
+    private func checkCost() {
+        while (totalCost() > maxCost) {
+            removeLastUsed()
+        }
+    }
+    
+    private func removeLastUsed() {
+        print("removeLastUsed()")
+        let node = lastUsed.remove(at: 0)
+    }
+    
+    public func describe() {
+        print("lastUsed : ")
+        for node in lastUsed {
+            node.describe()
+        }
+        
+        print("hash : ")
+        for key in hash.keys {
+            print("key -> \(key)")
+            if let node = hash[key] {
+                node.describe()
+            }
+        }
+        print("totalCost : \(totalCost())")
+    }
+}
+
+// Test
+
+// let n1 = Node<String, Int>(key:"ok", value:5, cost:1)
+// n.describe()
+
+let l = LRUCache<String, Int>(maxCost:10)
+l.insert(key:"ok", value:5, cost:1)
+l.describe()
+l.insert(key:"ok", value:5, cost:9)
+l.describe()
+l.insert(key:"ok", value:5, cost:2)
+l.describe()
+
+
+
+
