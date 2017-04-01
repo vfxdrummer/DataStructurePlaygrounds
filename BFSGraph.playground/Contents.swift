856662983637307822dbaa563a01d361b5ee4e1e
vfxdrummer <timothy.brandt@gmail.com>
2017-04-01 13:28:48 -0700

    Update with 4.01.17 self test

--- BFSGraph.playground/Contents.swift
+++ BFSGraph.playground/Contents.swift
@@ -2,4 +2,131 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class Node<T : Hashable> {
+  var value:T
+  var edges:[Edge<T>]
+  var visited:Bool
+  
+  init(value:T) {
+    self.value = value
+    self.edges = []
+    self.visited = false
+  }
+  
+  private func addEdge(edge:Edge<T>) {
+    self.edges.append(edge)
+  }
+  
+  func connectToNode(node:Node<T>) {
+    addEdge(edge:Edge(node:node))
+  }
+  
+  func describe() {
+    print("\(value)")
+    for edge in edges {
+      print("   \(edge.node.value)")
+    }
+  }
+  
+}
+
+class Edge<T : Hashable> {
+  var node:Node<T>
+  
+  init(node:Node<T>) {
+    self.node = node
+  }
+}
+
+class Graph<T : Hashable> {
+  var nodes:[T:Node<T>]
+  
+  init() {
+    self.nodes = [:]
+  }
+  
+  func addNode(value:T) -> Node<T> {
+    let node = Node(value:value)
+    self.nodes[value] = node
+    return node
+  }
+  
+  func connect(value1:T, value2:T) {
+    if let n1 = nodes[value1], let n2 = nodes[value2] {
+      n1.connectToNode(node:n2)
+    }
+  }
+  
+  func setUnVisited() {
+    for key in nodes.keys {
+      if let node = nodes[key] {
+        node.visited = false
+      }
+    }
+  }
+  
+  func BFS(start:T) {
+    var queue:[Node<T>] = []
+    setUnVisited()
+    if let node = nodes[start] {
+      queue.append(node)
+    }
+    while (queue.count > 0) {
+      let node = queue.removeFirst()
+      node.visited = true
+      print("Visit \(node.value)")
+      
+      for edge in node.edges {
+        if (edge.node.visited == false) {
+          print("   Add child \(edge.node.value)")
+          edge.node.visited = true
+          queue.append(edge.node)
+        }
+      }
+    }
+  }
+  
+  func describe() {
+    for key in nodes.keys {
+      if let node = nodes[key] {
+        node.describe()
+      }
+    }
+  }
+}
+
+// Test
+let g1 = Graph<Int>()
+let n1 = g1.addNode(value:5)
+let n2 = g1.addNode(value:10)
+let n3 = g1.addNode(value:15)
+let n4 = g1.addNode(value:20)
+let n5 = g1.addNode(value:25)
+let n6 = g1.addNode(value:30)
+let n7 = g1.addNode(value:35)
+let n8 = g1.addNode(value:40)
+
+n1.connectToNode(node:n2)
+n1.connectToNode(node:n3)
+n2.connectToNode(node:n3)
+n2.connectToNode(node:n4)
+n2.connectToNode(node:n8)
+n2.connectToNode(node:n5)
+n3.connectToNode(node:n4)
+n3.connectToNode(node:n1)
+n4.connectToNode(node:n1)
+n4.connectToNode(node:n2)
+n4.connectToNode(node:n8)
+n4.connectToNode(node:n7)
+n4.connectToNode(node:n6)
+n4.connectToNode(node:n5)
+n4.connectToNode(node:n2)
+
+// n1.describe()
+// n2.describe()
+// n3.describe()
+// n4.describe()
+
+g1.describe()
+
+g1.BFS(start:5)
--- Trie.playground/Contents.swift
+++ Trie.playground/Contents.swift
@@ -17,108 +17,235 @@ import UIKit
 // }
 // SNAP, SNAPCHAT, CHAT
 
-// Trie Data strcuture
-class TrieNode {
-  var character:Character?
-  var children:[TrieNode]
-  
-  init(character:Character?) {
-    self.character = character
-    self.children = []
-  }
-  
-  func findChar(char:Character) -> TrieNode? {
-    for child in children {
-      if (child.character == char) {
-        return child
-      }
-    }
-    return nil
-  }
-  
-  func getWords(partialWord:String) -> [String] {
-    var characterArrays:[String] = []
-    if (self.character == Character("1")) {
-      characterArrays.append(partialWord)
-    } else {
-      // partial string
-      let partialWordNew = partialWord + String(describing: self.character!)
-      for child in children {
-        for word in child.getWords(partialWord:partialWordNew) {
-          characterArrays.append( word )
-        }
-      }
-    }
-    return characterArrays
-  }
-  
-  func describe(indent:String) {
-    for child in children {
-      print("\(indent)\(child.character!)")
-      child.describe(indent:(indent + "  "))
-    }
-  }
-}
+//// Trie Data strcuture
+//class TrieNode {
+//  var character:Character?
+//  var children:[TrieNode]
+//  
+//  init(character:Character?) {
+//    self.character = character
+//    self.children = []
+//  }
+//  
+//  func findChar(char:Character) -> TrieNode? {
+//    for child in children {
+//      if (child.character == char) {
+//        return child
+//      }
+//    }
+//    return nil
+//  }
+//  
+//  func getWords(partialWord:String) -> [String] {
+//    var characterArrays:[String] = []
+//    if (self.character == Character("1")) {
+//      characterArrays.append(partialWord)
+//    } else {
+//      // partial string
+//      let partialWordNew = partialWord + String(describing: self.character!)
+//      for child in children {
+//        for word in child.getWords(partialWord:partialWordNew) {
+//          characterArrays.append( word )
+//        }
+//      }
+//    }
+//    return characterArrays
+//  }
+//  
+//  func describe(indent:String) {
+//    for child in children {
+//      print("\(indent)\(child.character!)")
+//      child.describe(indent:(indent + "  "))
+//    }
+//  }
+//}
+//
+//// SN
+////
+//
+//class Trie {
+//  var root:TrieNode
+//  
+//  init() {
+//    self.root = TrieNode(character:nil)
+//  }
+//  
+//  func insertWord(word:String) {
+//    var characters = word.characters
+//    print("here")
+//    characters.append(Character("1"))
+//    var node = root
+//    for character in characters {
+//      print(character)
+//      if let childNode = node.findChar(char:character) {
+//        print("found node \(childNode.character!)")
+//        node = childNode
+//      } else {
+//        let childNode = TrieNode(character: character)
+//        node.children.append(childNode)
+//        print("add node \(childNode.character!)")
+//        node = childNode
+//      }
+//    }
+//  }
+//  
+//  func searchWords(text:String) -> [String] {
+//    let characters = text.characters
+//    var autocompleteWords:[String] = []
+//    var node = root
+//    for character in characters {
+//      if let childNode = node.findChar(char:character) {
+//        node = childNode
+//      } else {
+//        return []
+//      }
+//    }
+//    // at last character of trie tree, print all complete words
+//    for childNode in node.children {
+//      for word in childNode.getWords(partialWord:"") {
+//        autocompleteWords.append(text + word)
+//      }
+//    }
+//    return autocompleteWords
+//  }
+//}
+//
+//let trie = Trie()
+//trie.insertWord(word: "snap")
+//trie.insertWord(word: "snapchat")
+//trie.insertWord(word: "snapchatter")
+//trie.root.describe(indent: "")
+//let words = trie.searchWords(text: "snap")
+//for word in words {
+//  print(word)
+//}
 
-// SN
+// test - 4.01.17
+
+// Trie
+//
+// Api
+//  - Insert word(s)
+//  - Search word(s)
+//  - Remove word(s)
+//  - Print word(s)
+//
+// TrieNode
+//  - value : character
+//  - next : dictionary - key : char, value : TrieNode?
+//
+//  - Insert(word)
+//      node = rootNode
+//      split word into character array, add null
+//      while(characters)
+//          - grab first character
+//              - if it's in next dictionary, node = next[char].trieNode
+//              - else, node = createNode(char), insert it in next
+//
+//  - Search(word)
+//      node = rootNode
+//      split word into character array, add null
+//      while(characters)
+//          - grab first character
+//              - if it's in next dictionary, node = next[char].trieNode
+//              - else, return false
+//
+//  - Remove word(s)
+//
+//  - Print words(currentString ="")
+//      node = rootNode
+//      currentString = ""
+//      for each key in next.keys
+//          if (key == null) {
+//              - print(currentString)
+//          else
+//              currentString += key // add character to current string
+//              printWords(currentString: currentString)
+//
 //
 
-class Trie {
-  var root:TrieNode
-  
-  init() {
-    self.root = TrieNode(character:nil)
-  }
-  
-  func insertWord(word:String) {
-    var characters = word.characters
-    print("here")
-    characters.append(Character("1"))
-    var node = root
-    for character in characters {
-      print(character)
-      if let childNode = node.findChar(char:character) {
-        print("found node \(childNode.character!)")
-        node = childNode
-      } else {
-        let childNode = TrieNode(character: character)
-        node.children.append(childNode)
-        print("add node \(childNode.character!)")
-        node = childNode
-      }
+
+class TrieNode {
+    public var value : Character
+    public var next : [Character:TrieNode]
+    
+    init(value:Character) {
+        self.value = value
+        self.next = [:]
     }
-  }
-  
-  func searchWords(text:String) -> [String] {
-    let characters = text.characters
-    var autocompleteWords:[String] = []
-    var node = root
-    for character in characters {
-      if let childNode = node.findChar(char:character) {
-        node = childNode
-      } else {
-        return []
-      }
+    
+    public func printWords(currentString: String) {
+        for character in self.next.keys {
+            if (character == "\n") {
+                print("WORD : \(currentString)")
+            } else {
+                let newStr = currentString + String(character)
+                let node = self.next[character]!
+                node.printWords(currentString: newStr)
+            }
+        }
     }
-    // at last character of trie tree, print all complete words
-    for childNode in node.children {
-      for word in childNode.getWords(partialWord:"") {
-        autocompleteWords.append(text + word)
-      }
-    }
-    return autocompleteWords
-  }
 }
 
-let trie = Trie()
-trie.insertWord(word: "snap")
-trie.insertWord(word: "snapchat")
-trie.insertWord(word: "snapchatter")
-trie.root.describe(indent: "")
-let words = trie.searchWords(text: "snap")
-for word in words {
-  print(word)
+class Trie {
+    private var root:TrieNode
+    
+    init() {
+        root = TrieNode(value: "\0")
+    }
+    
+    public func insert(word: String) {
+        var characters = word.characters
+        characters.append("\n")
+        var node:TrieNode = root
+        print(node)
+        for character in characters {
+            if let nextNode = node.next[character] as TrieNode! {
+                node = nextNode
+            } else {
+                print("insert \(character)")
+                let newNode = TrieNode(value: character)
+                node.next[character] = newNode
+                node = newNode
+            }
+        }
+    }
+    
+    
+    public func search(word: String) -> Bool {
+        var characters = word.characters
+        characters.append("\n")
+        var node:TrieNode = root
+        print(node)
+        for character in characters {
+            if let nextNode = node.next[character] as TrieNode! {
+                node = nextNode
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    public func printWords() {
+        root.printWords(currentString: "")
+    }
 }
+
+
+// Test
+let n = TrieNode(value: "\0")
+print(n.value)
+
+let t = Trie()
+
+t.insert(word:"snap")
+t.insert(word:"snapchat")
+t.printWords()
+
+print(t.search(word:"snap"))
+print(t.search(word:"snapchat"))
+print(t.search(word:"snapchatz"))
 
 
 
